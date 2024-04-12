c3a596f3c67976cbeaeec0c172a0d806a3625b96
İsmail Tapan <ismltpn@gmail.com>
2024-04-12 18:04:21 +0300

    add Aqua.jl to test module

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,11 @@
 using Polynomials
+using Aqua
 using Test
 
 @testset "Polynomials.jl" begin
     # Write your tests here.
+end
+
+@testset "Polynomials.jl Aqua" begin
+    Aqua.test_all(AlcyonValue)
 end
