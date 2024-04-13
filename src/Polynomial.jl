8077a777291cdd13818d2f51c2390e2b07838e8a
İsmail Tapan <ismltpn@gmail.com>
2024-04-13 15:35:09 +0300

    fix type error, version: 1.0.2-DEv

--- src/Polynomial.jl
+++ src/Polynomial.jl
@@ -2,7 +2,7 @@ using AbstractAlgebra
 
 const Polynomial{T} = AbstractAlgebra.Generic.MPoly{Rational{T}}
 
-function create_monomial_from_exponents(z::Vector{T}, variables::Int)::Polynomial{T} where {T<:Number}
+function create_monomial_from_exponents(z::Vector{<:Int}, variables::Int)::Polynomial{BigInt}
     VV = [string("x", i) for i in 1:variables]
     S, X = polynomial_ring(QQ, VV)
     tmp = 1
