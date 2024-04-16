8491d107dc646a1b64c28b111802e5583a448ed8
İsmail Tapan <ismltpn@gmail.com>
2024-04-16 17:29:07 +0300

    change to Symbolics, version: 1.0.4-DEV

--- src/CombinationOfRationalFunctions.jl
+++ src/CombinationOfRationalFunctions.jl
@@ -1,38 +1,41 @@
-struct CombinationOfRationalFunctions{T}
-    ratfuns::Vector{Pair{Polynomial{T},Polynomial{T}}}
+struct CombinationOfRationalFunctions
+    ratfuns::Vector{Pair{Polynomial,Polynomial}}
 
-    function CombinationOfRationalFunctions(ratfuns::Vector{Pair{Polynomial{T},Polynomial{T}}}) where {T}
-        new{T}(ratfuns)
+    function CombinationOfRationalFunctions(ratfuns::Vector{Pair{Polynomial,Polynomial}})
+        new(ratfuns)
     end
-    function CombinationOfRationalFunctions(ratfun::Pair{Polynomial{T},Polynomial{T}}) where {T}
-        new{T}([ratfun])
+    function CombinationOfRationalFunctions(ratfun::Pair{Polynomial,Polynomial})
+        new([ratfun])
     end
 
-    function CombinationOfRationalFunctions{T}() where {T}
-        new(Vector{Pair{Polynomial{T},Polynomial{T}}}([]))
+    function CombinationOfRationalFunctions()
+        new(Vector{Pair{Polynomial,Polynomial}}([]))
     end
 end
 
-function Base.:(+)(combination::CombinationOfRationalFunctions{T}, ratfun::Pair{Polynomial{T},Polynomial{T}}) where {T}
+function Base.:(+)(combination::CombinationOfRationalFunctions, ratfun::Pair{Polynomial,Polynomial})
     # TODO: DO in place instead
-    #newratfuns::Vector{Pair{Cone{T},Int}} = []
+    ## Why we need to do this in place?
+    newpairs = Vector{Pair{Polynomial,Polynomial}}([])
     found = false
     for (numerator, denominator) in combination.ratfuns
         # This is potentially very expensive computationally
-        if denominator == ratfun[2] # check if this expands. if so, consider alternstive representation.
-            ratfun[1] += numerator
+        if isequal(denominator, ratfun[2]) # check if this expands. if so, consider alternstive representation.
+            push!(newpairs, Pair(numerator + ratfun[1], denominator))
             found = true
+        else
+            push!(newpairs, Pair(numerator, denominator))
         end
     end
     if !found
-        # push!(newratfuns, ratfun)
-        push!(combination.ratfuns, ratfun)
+        push!(newpairs, ratfun)
+        # push!(combination.ratfuns, ratfun)
     end
-    return combination
+    return CombinationOfRationalFunctions(newpairs)
 end
 
 
-function Base.:(+)(combination1::CombinationOfRationalFunctions{<:T}, combination2::CombinationOfRationalFunctions{<:T}) where {T}
+function Base.:(+)(combination1::CombinationOfRationalFunctions, combination2::CombinationOfRationalFunctions)
     combination = CombinationOfRationalFunctions(combination1.ratfuns)
     for pair in combination2.ratfuns
         combination += pair
@@ -40,8 +43,8 @@ function Base.:(+)(combination1::CombinationOfRationalFunctions{<:T}, combinatio
     return combination
 end
 
-function Base.:(*)(combination::CombinationOfRationalFunctions{T}, c::Integer) where {T}
-    newpairs = Vector{Pair{Polynomial{T},Polynomial{T}}}([])
+function Base.:(*)(combination::CombinationOfRationalFunctions, c::Integer)
+    newpairs = Vector{Pair{Polynomial,Polynomial}}([])
     for ratfun in combination.ratfuns
         push!(newpairs, Pair(ratfun[1] * c, ratfun[2]))
     end
@@ -63,9 +66,13 @@ function subs(c::CombinationOfRationalFunctions, v::Vector)
     return sum([r(v) for r in R])
 end
 
-function Base.show(io::IO, c::CombinationOfRationalFunctions{T}) where {T}
-    println(io, "CombinationOfRationalFunctions{::$(T)}: ")
-    for ratfun in c.ratfuns
-        println(io, "\t$(summary(ratfun))")
+function Base.show(io::IO, c::CombinationOfRationalFunctions)
+    print(io, "CombinationOfRationalFunctions: ")
+    for (i, ratfun) in enumerate(c.ratfuns)
+        if (i == length(c.ratfuns))
+            print(io, "(($(ratfun[1]))/($(ratfun[2])))")
+        else
+            print(io, "(($(ratfun[1]))/($(ratfun[2]))) + ")
+        end
     end
 end
--- src/Polynomial.jl
+++ src/Polynomial.jl
@@ -1,10 +1,9 @@
-using AbstractAlgebra
+using Symbolics
 
-const Polynomial{T} = AbstractAlgebra.Generic.MPoly{Rational{T}}
+const Polynomial = Num
 
-function create_monomial_from_exponents(z::Vector{<:Number}, variables::Int)::Polynomial{BigInt}
-    VV = [string("x", i) for i in 1:variables]
-    S, X = polynomial_ring(QQ, VV)
+function create_monomial_from_exponents(z::Vector{<:Number}, variables::Int)::Polynomial
+    @variables X[1:variables]
     tmp = 1
     for i in eachindex(z)
         tmp = tmp * (X[i]^Int(z[i]))
