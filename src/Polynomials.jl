4cc7b7eac04a7c21e20fae54498f6ebfc3905595
İsmail Tapan <ismltpn@gmail.com>
2024-04-16 22:53:58 +0300

    update exports, re-export simplify, version: 1.0.7-DEV

--- src/Polynomials.jl
+++ src/Polynomials.jl
@@ -1,8 +1,18 @@
 module Polynomials
-
+using Symbolics
 include("Polynomial.jl")
 include("CombinationOfRationalFunctions.jl")
 
-export CombinationOfRationalFunctions, Polynomial, get_ratfun, subs, create_monomial_from_exponents
+export
+    # Types
+    CombinationOfRationalFunctions, Polynomial,
+    # Functions
+    get_ratfun,
+    create_monomial_from_exponents,
+    get_variable_array,
+    evaluate,
+    evaluate_all_with,
+    simplify
+
 
 end
