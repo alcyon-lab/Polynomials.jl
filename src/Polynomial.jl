a36e37ca6f85c941100cc122fc89ab72a8d83ebd
İsmail Tapan <ismltpn@gmail.com>
2024-06-03 15:58:12 +0300

    version 1.0.9-DEV, get variables from outside

--- src/Polynomial.jl
+++ src/Polynomial.jl
@@ -2,16 +2,11 @@ using Symbolics
 
 const Polynomial = Num
 
-function create_monomial_from_exponents(z::Vector{<:Number}, variables::Int)::Polynomial
-    x = get_variable_array(variables)
+function create_monomial_from_exponents(z::Vector{<:Number}, x::Union{Vector})::Polynomial
+    assert(length(z) <= length(x))
     tmp = 1
     for i in eachindex(z)
         tmp = tmp * (x[i]^Int(z[i]))
     end
     return tmp
-end
-
-function get_variable_array(variables::Int)
-    @variables x[1:variables]
-    return x
 end
